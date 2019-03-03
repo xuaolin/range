b7d9214e57cdbc1be703a33ea735fd0b65123532
xuaolin <47988734+xuaolin@users.noreply.github.com>
2019-03-02 18:02:51 -0600

    Add files via upload

--- range.py
+++ range.py
@@ -4,3 +4,6 @@ range(3) #[0, 1, 2]
 for i in range(3):
 	r = random.randint(1,1000)
 	print(r) #generate random number from 1-1000 range for three times
+
+range(2, 5) # [2, 3, 4]
+range(2, 10, 3) # begin with 3, step is 3, the end is near 10, [2, 5, 8]
